81b94d584c2b8ebe490feee607c85869392e1e0f
Mohammednur <mamebb2023@gmail.com>
2024-02-20 10:01:15 +0300

    Express Server

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import routerController from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+routerController(app);
+
+app.listen(port, () => {
+  console.log('Server running on port:', port);
+});
+
+export default app;
