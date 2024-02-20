fd9210d57bf92c3fc0e4d47a014f2e6b7e0ad0fa
Mohammednur <mamebb2023@gmail.com>
2024-02-20 11:21:41 +0300

    Main Routes

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import path from 'path';
+
 import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
 const routerController = (app) => {
+  app.use('/assets', express.static(path.resolve(__dirname, '../templates/assets')));
   app.use('/', router);
 
   // Templates
   router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../templates/index.html')));
+  router.get('/about', (req, res) => res.sendFile(path.resolve(__dirname, '../templates/about.html')));
 
   // App Controller
   router.get('/status', (req, res) => AppController.getStatus(req, res));
