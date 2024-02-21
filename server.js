25eff7bed8c9e534c052c7e7d741981c60b1091e
Mohammednur <mamebb2023@gmail.com>
2024-02-21 09:17:36 +0300

    Express Server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import routerController from './routes/index';
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 routerController(app);
