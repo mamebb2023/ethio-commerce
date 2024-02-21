4cff4781e96437ad9fd48797a93103d583c0e3e5
Mohammednur <mamebb2023@gmail.com>
2024-02-21 21:40:09 +0300

    Main Routes

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,14 @@ import UserController from '../controllers/UserController';
 const router = express.Router();
 
 const routerController = (app) => {
-  app.use(express.static(path.resolve(__dirname, '../views'))); 
-  app.set('view engine', 'hbs');
+  app.use(express.static(path.resolve(__dirname, '../views')));
   app.use('/', router);
 
   // Templates
-  router.get('/', (req, res) => res.render('index'));
-  router.get('/about', (req, res) => res.render('about'));
-  router.get('/register', (req, res) => res.render('register'));
-  router.get('/login', (req, res) => res.render('login'));
+  router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../views/index.html')));
+  router.get('/about', (req, res) => res.sendFile(path.resolve(__dirname, '../views/about.html')));
+  router.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, '../views/register.html')));
+  router.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, '../views/login.html')));
 
   // App Controller
   router.get('/status', (req, res) => AppController.getStatus(req, res));
