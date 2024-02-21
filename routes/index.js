52144ce4cd9b6ac6e0fd87d174b39e9f1e31dad1
Mohammednur <mamebb2023@gmail.com>
2024-02-21 13:14:43 +0300

    Main Routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 
 import AppController from '../controllers/AppController';
+import UserController from '../controllers/UserController';
 
 const router = express.Router();
 
@@ -13,11 +14,16 @@ const routerController = (app) => {
   // Templates
   router.get('/', (req, res) => res.render('index'));
   router.get('/about', (req, res) => res.render('about'));
+  router.get('/register', (req, res) => res.render('register'));
   router.get('/login', (req, res) => res.render('login'));
 
   // App Controller
   router.get('/status', (req, res) => AppController.getStatus(req, res));
   router.get('/stats', (req, res) => AppController.getStats(req, res));
+
+  // User Controller
+  router.post('/login', (req, res) => UserController.postLogin(req, res));
+  router.post('/register', (req, res) => UserController.postRegister(req, res));
 };
 
 export default routerController;
