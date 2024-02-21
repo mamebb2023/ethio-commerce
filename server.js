95e59ced49bf6b8054916047954f623dae58247b
Mohammednur <mamebb2023@gmail.com>
2024-02-21 21:39:40 +0300

    Server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import routerController from './routes/index';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({extended: 'false'}));
+app.use(express.urlencoded({ extended: 'false' }));
 app.use(express.json());
 routerController(app);
 
