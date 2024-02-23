7d8fa341c2662f91fed11371f517f917049bc0d0
Mohammednur <mamebb2023@gmail.com>
2024-02-23 12:03:36 +0300

    User Utils

--- utils/user.js
+++ utils/user.js
@@ -1,10 +1,21 @@
 import { ObjectId } from 'mongodb';
 import bcrypt from 'bcrypt';
+import { v4 } from 'uuid';
 
 import redisClient from './redis';
 import dbClient from './db';
 
 class userUtils {
+  static async createToken(userId) {
+    const token = v4();
+    const key =  `auth_${token}`;
+    const expirationHour = 24;
+
+    await redisClient.set(key, userId.toString(), expirationHour);
+
+    return token;
+  }
+
   static async getUserIdAndKey(req) {
     const xToken = req.header('X-Token');
     if (!xToken) return {};
@@ -30,7 +41,7 @@ class userUtils {
   }
 
   static encryptPwd(password, salt) {
-    const hashedpwd =  bcrypt.hash(password, salt);
+    const hashedpwd = bcrypt.hash(password, salt);
     return hashedpwd;
   }
 }
