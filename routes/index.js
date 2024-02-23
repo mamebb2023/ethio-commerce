316c4ed4a9aaf25311fc3a5db5cd94c170f4c49c
Mohammednur <mamebb2023@gmail.com>
2024-02-23 12:03:24 +0300

    Main Routes

--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,19 @@ const routerController = (app) => {
 
   // Templates
   router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../views/index.html')));
+  router.get('/about', (req, res) => res.sendFile(path.resolve(__dirname, '../views/about.html')));
   router.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, '../views/register.html')));
   router.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, '../views/login.html')));
+
+  router.get('/user/me', (req, res) => res.sendFile(path.resolve(__dirname, '../views/user_panel.html')));
 
   // App Controller
   router.get('/status', (req, res) => AppController.getStatus(req, res));
   router.get('/stats', (req, res) => AppController.getStats(req, res));
 
   // User Controller
-  router.post('/login', (req, res) => UserController.postLogin(req, res));
-  router.post('/register', (req, res) => UserController.postRegister(req, res));
-
-  // Auth Controller
-  router.post('/validate', (req, res) => AuthController.validateUser(req, res));
+  router.post('/login', (req, res) => UserController.userLogin(req, res));
+  router.post('/register', (req, res) => UserController.userRegister(req, res));
 };
 
 export default routerController;
