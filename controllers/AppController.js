7687873230ba41db6e16e29baa51e3ce70628baa
Mohammednur <mamebb2023@gmail.com>
2024-02-27 09:56:14 +0300

    App Controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -6,16 +6,10 @@ class AppController {
     const status = {
       redis: redisClient.isAlive(),
       db: redisClient.isAlive(),
+      users: dbClient.nbUsers(),
+      items: dbClient.nbItems(),
     };
     res.status(200).send(status);
-  }
-
-  static async getStats(req, res) {
-    const stats = {
-      users: dbClient.nbUsers(),
-      files: dbClient.nbFiles(),
-    };
-    res.status(200).send(stats);
   }
 }
 
