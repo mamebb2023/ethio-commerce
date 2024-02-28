2dc0ec4aef08517e1b7223b2ca9e320362f78090
Mohammednur <mamebb2023@gmail.com>
2024-02-28 12:48:50 +0300

    Main Routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,34 @@
 import express from 'express';
 import path from 'path';
+import multer from 'multer';
 
+import userUtils from '../utils/user';
 import AppController from '../controllers/AppController';
 import UserController from '../controllers/UserController';
 import AuthController from '../controllers/AuthController';
-import userUtils from '../utils/user';
+import ItemController from '../controllers/ItemControler';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Replace with your desired upload directory
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now();
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+  }
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 1000000, // 1 MB limit (adjust as needed)
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedExtensions = ['.jpg', '.jpeg', '.png']; // Allowed extensions
+    const extname = path.extname(file.originalname);
+    cb(null, allowedExtensions.includes(extname));
+  }
+});
 
 const router = express.Router();
 
@@ -22,19 +46,20 @@ const routerController = (app) => {
   router.get('/admin', AuthController.verifyUser, userUtils.isAdmin, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../views/admin.html'));
   });
-  router.post('/postItem', AuthController.verifyUser, userUtils.isAdmin, (req, res) => {
-    ItemController.postItem(req, res);
-  });
+
+  router.post('/post-item', AuthController.verifyUser, userUtils.isAdmin, upload.single('itemImage'),
+    async (req, res) => ItemController.postItem(req, res));
+
   router.get('/user/me', AuthController.verifyUser, (req, res) => { 
     res.sendFile(path.resolve(__dirname, '../views/user.html'));
   });
+
   router.get('/user/cart', AuthController.verifyUser, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../views/cart.html'));
   });
 
   // App Controller
   router.get('/status', (req, res) => AppController.getStatus(req, res));
-  router.get('/stats', (req, res) => AppController.getStats(req, res));
 
   // User Controller
   router.post('/login', (req, res) => UserController.userLogin(req, res));
