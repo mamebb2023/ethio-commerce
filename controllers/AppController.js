815404b792ef05734c678019f025c022cf7c685e
Mohammednur <mamebb2023@gmail.com>
2024-02-28 12:46:59 +0300

    App Controller

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,12 +2,12 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, res) {
+  static async getStatus(req, res) {
     const status = {
-      redis: redisClient.isAlive(),
-      db: redisClient.isAlive(),
-      users: dbClient.nbUsers(),
-      items: dbClient.nbItems(),
+      redis: await redisClient.isAlive(),
+      db: await dbClient.isAlive(),
+      users: await dbClient.nbUsers(),
+      items: await dbClient.nbItems(),
     };
     res.status(200).send(status);
   }
