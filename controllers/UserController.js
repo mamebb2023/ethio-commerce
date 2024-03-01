6e2443e05e1ed67b79bf88b51043ede1aa861012
Mohammednur <mamebb2023@gmail.com>
2024-03-01 20:43:35 +0300

    User Controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,13 +11,13 @@ class UserController {
       firstName, lastName, email, password,
     } = req.body;
    
-    if (!firstName) return res.status(400).send({ error: 'First name required' });
-    if (!lastName) return res.status(400).send({ error: 'Last name required' });
-    if (!email) return res.status(400).send({ error: 'Email required' });
-    if (!password) return res.status(400).send({ error: 'Password required' });
+    if (!firstName) return res.status(200).send({ error: 'First name required' });
+    if (!lastName) return res.status(200).send({ error: 'Last name required' });
+    if (!email) return res.status(200).send({ error: 'Email required' });
+    if (!password) return res.status(200).send({ error: 'Password required' });
 
     const user = await userUtils.getUser({ email }, { projection: { password: false } });
-    if (user) return res.status(400).send({ error: 'Email Already Exists' });
+    if (user) return res.status(200).send({ error: 'Email Already Exists' });
 
     // const salt = 10;
     // const hashPassword = bcrypt.hash(password, salt);
@@ -42,17 +42,17 @@ class UserController {
       email, password, redirectUrl
     } = req.body;
 
-    if (!email) return res.status(400).send({ error: 'Email required' });
-    if (!password) return res.status(400).send({ error: 'Password required' });
+    if (!email) return res.status(200).send({ error: 'Email required' });
+    if (!password) return res.status(200).send({ error: 'Password required' });
 
     const user = await userUtils.getUser({ email });
-    if (!user) return res.status(400).send({ error: 'Invalid email or password' });
+    if (!user) return res.status(200).send({ error: 'Invalid email or password' });
 
     try {
       // const isValid = bcrypt.compare(password, user.password);
       const hashPassword = sha1(password);
       if (hashPassword !== user.password) {
-        return res.status(400).send({ error: 'Invalid email or password' });
+        return res.status(200).send({ error: 'Invalid email or password' });
       }
       delete user.password;
 
