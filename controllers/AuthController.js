3f948e83c0872f998cd677a4e0db3743f7e4a2ca
Mohammednur <mamebb2023@gmail.com>
2024-03-01 20:25:35 +0300

    Auth Controller

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -12,25 +12,24 @@ class AuthController {
             ?.find(cookie => cookie.startsWith('X-Token='))
             ?.split('=')[1];
         if (!token) {
-            return res.status(401).send({ error: 'Unauthorized' });
+            return res.status(200).send({ error: 'Unauthorized' });
         }
 
         const key = `auth_${token}`;
 
         try {
             const userId = await redisClient.get(key);
-            if (!userId) return res.status(401).send({ error: 'Unauthorized' });
+            if (!userId) return res.status(400).send({ error: 'Unauthorized' });
 
             const userObjId = ObjectId(userId);
             const user = await userUtils.getUser({ _id: userObjId }, { projection: { password: false } });
-            if (!user) return res.status(401).send({ error: 'Unauthorized' });
+            if (!user) return res.status(400).send({ error: 'Unauthorized' });
 
             req.user = { user, key, redirectUrl };
-            console.log('verifyUser', req.user);
             next();
         } catch (error) {
             console.log(error);
-            return res.status(401).send({ error: 'Unauthorized' });
+            return res.status(400).send({ error: 'Unauthorized' });
         }
     }
 }
