9b473f524bfed1eef6d20b05637f0dcdfa632016
Mohammednur <mamebb2023@gmail.com>
2024-03-06 09:49:23 +0300

    User Controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -5,12 +5,21 @@ import dbClient from '../utils/db';
 import userUtils from '../utils/user';
 import redisClient from '../utils/redis';
 
+/**
+ * User Controller
+ */
 class UserController {
+  /**
+   * A function to register a user
+   * @param {*} req Request form user
+   * @param {*} res Response sent to user
+   * @returns A response with status code and a json message
+   */
   static async userRegister(req, res) {
     const {
       firstName, lastName, email, password,
     } = req.body;
-   
+
     if (!firstName) return res.status(200).send({ error: 'First name required' });
     if (!lastName) return res.status(200).send({ error: 'Last name required' });
     if (!email) return res.status(200).send({ error: 'Email required' });
@@ -37,9 +46,15 @@ class UserController {
     }
   }
 
+  /**
+   * A function to login a registered user
+   * @param {*} req Request form user
+   * @param {*} res Response sent to user
+   * @returns A response with status code and a json message
+   */
   static async userLogin(req, res) {
     const {
-      email, password, redirectUrl
+      email, password, redirectUrl,
     } = req.body;
 
     if (!email) return res.status(200).send({ error: 'Email required' });
@@ -65,35 +80,47 @@ class UserController {
       });
       return res.status(200).send({ msg: 'You are logged in!', token, redirectUrl });
     } catch (error) {
-      console.log(err);
+      console.log(error);
       return res.status(500).send({ error: 'Internal Server Error' });
     }
   }
 
+  /**
+   * Gets the current user
+   * @param {*} req Request form user
+   * @param {*} res Response sent to user
+   * @returns A response with status code and the user
+   */
   static getUser(req, res) {
     delete req.user.key;
     if (!req.user.user) return res.status(400).send({ error: 'Unauthorized' });
 
     delete req.user.user._id;
 
-    const user = req.user.user;
+    const { user } = req.user;
     console.log('getUser', req.user.user);
 
     return res.status(201).send(user);
   }
 
+  /**
+   * A function to log out a logged in user
+   * @param {*} req Request form user
+   * @param {*} res Response sent to user
+   * @returns A response with status code and a redirection to the main page
+   */
   static async userLogout(req, res) {
-    const key = req.user.key;
+    const { key } = req.user;
     if (!key) return res.status(400).send({ error: 'Unauthorized 6' });
 
-    try{
+    try {
       await redisClient.del(key);
       res.clearCookie('X-Token');
       delete req.key;
       delete req.user;
       return res.status(201).send({ redirectUrl: '/' });
     } catch (error) {
-      return res.status(500).send({ error: 'Internal Server Error' })
+      return res.status(500).send({ error: 'Internal Server Error' });
     }
   }
 }
